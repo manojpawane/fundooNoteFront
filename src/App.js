c2c2c4cf70ee5a8318dc7d51f1ac85f401d3f716
Manoj-Pawane <manojpawane@gmail.com>
2019-01-02 17:55:29 +0530

    code optimized

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
 import React, { Component } from 'react';
 import Login from '../src/components/Login/Login'
 import Register from '../src/components/Registration/Registration'
-//const BrowserRouter = require('react-router-dom').BrowserRouter
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-// const Route = require('react-router-dom').Route
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div className="App">
-
           <div className="container">
             <Route exact path="/" component={Register} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
-
           </div>
         </div>
       </Router>
