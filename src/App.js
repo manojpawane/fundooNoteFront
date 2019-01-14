5f8b70e9a864680c8bdc2beeaea3dd1574f0066a
Manoj-Pawane <manojpawane@gmail.com>
2019-01-14 19:42:33 +0530

    coded added

--- src/App.js
+++ src/App.js
@@ -5,23 +5,42 @@ import VerifyToken from '../src/components/VerifyToken/VerifyToken'
 import Dashboard from '../src/components/Dashboard/Dashboard'
 import ForgetPassword from '../src/components/ForgetPassword/ForgetPassword'
 import UpdateNewPassword from '../src/components/ForgetPassword/UpdateNewPassword'
-import Header from '../src/components/Header/Header'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 
 class App extends Component {
   render() {
+
+    function PrivateRoute({ component: Component, ...rest }) {
+      return (
+        <Route
+          {...rest}
+          render={props =>
+            localStorage.usertoken ? (
+              <Component {...props} />
+            ) : (
+              <Redirect
+                to={{
+                  pathname: "/login",
+                  state: { from: props.location }
+                }}
+              />
+            )
+          }
+        />
+      );
+    }
+
     return (
       <Router>
         <div className="App">
-        {localStorage.usertoken && <Header/> }
-          <div className="container">
+          <div>
             <Route exact path="/" component={Login} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/confirmation/:token" component={VerifyToken}/>
-            <Route exact path="/dashboard" component={Dashboard}/>
             <Route exact path="/forget" component={ForgetPassword}/>
             <Route exact path="/updatePassword/:token" component={UpdateNewPassword}/>
+            <PrivateRoute path="/dashboard" component={Dashboard} />
           </div>
         </div>
       </Router>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,46 +1,178 @@
-import React, { Component } from 'react'
+import React,{Component} from 'react';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import IconButton from '@material-ui/core/IconButton';
+import Typography from '@material-ui/core/Typography';
+import InputBase from '@material-ui/core/InputBase';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import { fade } from '@material-ui/core/styles/colorManipulator';
+import { withStyles } from '@material-ui/core/styles';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import AccountCircle from '@material-ui/icons/AccountCircle';
 import { withRouter } from 'react-router-dom'
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {Colors} from 'material-ui/styles/colors';
 
-/// Navbar is component which displays and manipulate the navigation bar
+const styles = theme => ({
+    root: {
+        width: '100%',
+        color:'white'
+    },
+    grow: {
+        flexGrow: 1,
+    },
+    menuButton: {
+        marginLeft: -12,
+        marginRight: 20,
+    },
+    title: {
+        display: 'none',
+        [theme.breakpoints.up('sm')]: {
+            display: 'block',
+        },
+    },
+    search: {
+        position: 'relative',
+        borderRadius: theme.shape.borderRadius,
+        backgroundColor: fade(theme.palette.common.white, 0.15),
+        '&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.25),
+        },
+        marginRight: theme.spacing.unit * 2,
+        marginLeft: 0,
+        width: '100%',
+        [theme.breakpoints.up('sm')]: {
+            marginLeft: theme.spacing.unit * 3,
+            width: 'auto',
+        },
+    },
+    searchIcon: {
+        width: theme.spacing.unit * 9,
+        height: '100%',
+        position: 'absolute',
+        pointerEvents: 'none',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    inputRoot: {
+        color: 'inherit',
+        width: '100%',
+    },
+    inputInput: {
+        paddingTop: theme.spacing.unit,
+        paddingRight: theme.spacing.unit,
+        paddingBottom: theme.spacing.unit,
+        paddingLeft: theme.spacing.unit * 10,
+        transition: theme.transitions.create('width'),
+        width: '100%',
+        [theme.breakpoints.up('md')]: {
+            width: 200,
+        },
+    },
+    sectionDesktop: {
+        display: 'none',
+        [theme.breakpoints.up('md')]: {
+            display: 'flex',
+        },
+    },
+});
+const muiTheme = getMuiTheme({
+    palette: {
+      textColor: Colors.darkBlack,
+      primary1Color: Colors.white,
+      primary2Color: Colors.indigo700,
+      accent1Color: Colors.redA200,
+      pickerHeaderColor: Colors.darkBlack,
+      alternateTextColor: Colors.redA200
+    },
+    appBar: {
+      height: 60,
+    },
+  });
 class Header extends Component {
+    state = {
+        anchorEl: null,
+    };
+
+    handleProfileMenuOpen = event => {
+        this.setState({ anchorEl: event.currentTarget });
+    };
+
     logOut(e) {
         e.preventDefault()
         localStorage.removeItem('usertoken')
         this.props.history.push('/');
     }
+    handleMenuClose = () => {
+        this.setState({ anchorEl: null });
+    };
 
     render() {
-        return (
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <button className="navbar-toggler"
-                    type="button"
-                    data-toggle="collapse"
-                    data-target="#navbar1"
-                    aria-controls="navbar1"
-                    aria-expanded="false"
-                    aria-label="Toggle-navigation">
-                    <span className="navbar-toggler-icon"></span>
-                    ></button>
+        const { anchorEl } = this.state;
+        const { classes } = this.props;
+        const isMenuOpen = Boolean(anchorEl);
 
-                <div className="collapse navbar-collapse justify-content-md-center" id="navbar1">
-                    <form className="form-inline my-2 my-lg-0">
-                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-                    </form>
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item dropdown">
-                            <button className="nav-link dropdown-toggle" id="navbarDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Profile
-                            </button>
-                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <button className="dropdown-item" onClick={this.logOut.bind(this)}>Logout</button>
+        const renderMenu = (
+            <Menu
+                anchorEl={anchorEl}
+                anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+                transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+                open={isMenuOpen}
+                onClose={this.handleMenuClose}
+            >
+                <MenuItem onClick={this.logOut.bind(this)}>Logout</MenuItem>
+            </Menu>
+        );
+
+        return (
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+                            Keep
+            </Typography>
+                        <div className={classes.search}>
+                            <div className={classes.searchIcon}>
+                                <SearchIcon />
                             </div>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        )
+                            <InputBase
+                                placeholder="Search…"
+                                classes={{
+                                    root: classes.inputRoot,
+                                    input: classes.inputInput,
+                                }}
+                            />
+                        </div>
+                        <div className={classes.grow} />
+                        <div className={classes.sectionDesktop}>
+                            <IconButton
+                                aria-owns={isMenuOpen ? 'material-appbar' : undefined}
+                                aria-haspopup="true"
+                                onClick={this.handleProfileMenuOpen}
+                                color="inherit"
+                            >
+                                <AccountCircle />
+                            </IconButton>
+                        </div>
+
+                    </Toolbar>
+                </AppBar>
+                {renderMenu}
+                
+            </div>
+        );
     }
 }
 
-export default withRouter(Header)
+Header.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withRouter((withStyles(styles)(Header)))
