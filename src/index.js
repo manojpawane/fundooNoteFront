31abdbf1ab5cfa28d93267f36175af09a22b761d
Manoj-Pawane <manojpawane@gmail.com>
2019-01-19 17:22:08 +0530

    cod eadded

--- src/index.js
+++ src/index.js
@@ -10,10 +10,7 @@ import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 // const store = configureStore()
 
 ReactDOM.render(
-    // <Provider store={store}>
-        <App />
-    // </Provider>
-    , document.getElementById('root'));
+    <App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
