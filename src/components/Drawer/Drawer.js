1cf867aa9f1a857d754726f9661565cb52fc986e
Manoj <manojpawane@gmail.com>
2019-02-04 19:19:31 +0530

    minor changes

--- src/components/Drawer/Drawer.js
+++ src/components/Drawer/Drawer.js
@@ -11,13 +11,15 @@ import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
+import Notes from '@material-ui/icons/Notes'
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
 import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
+import Delete from '@material-ui/icons/Delete';
+import Alarm  from '@material-ui/icons/Alarm';
 import { withRouter } from 'react-router-dom'
 import SearchIcon from '@material-ui/icons/Search';
 import InputBase from '@material-ui/core/InputBase';
@@ -284,21 +286,26 @@ handleMenuClose = () => {
           </div>
           <Divider />
           <List>
-            {['Notes', 'Reminders'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
+          <ListItem button key='Notes'>
+            <ListItemIcon><Notes/></ListItemIcon>
+            <ListItemText>Notes</ListItemText>
+            </ListItem>
+            <ListItem button key='Reminder'>
+            <ListItemIcon><Alarm/></ListItemIcon>
+            <ListItemText>Reminder</ListItemText>
+            </ListItem>
           </List>
           <Divider />
           <List>
-            {['Archive', 'Trash'].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
+            
+      <ListItem button key='Archive'>
+            <ListItemIcon><InboxIcon/></ListItemIcon>
+            <ListItemText>Archive</ListItemText>
+            </ListItem>
+            <ListItem button key='Delete'>
+            <ListItemIcon><Delete/></ListItemIcon>
+            <ListItemText>Trash</ListItemText>
+            </ListItem>
           </List>
         </Drawer>
         <main className={classes.content}>
